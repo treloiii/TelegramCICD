96b6c3aefe1052c95ff5b4a7a5e542e53d08001e
trelloiii <nikkraew22@gmail.com>
2020-06-13 19:47:16 +0300

    second props

--- src/main/java/com/trelloiii/cibot/CiBotApplication.java
+++ src/main/java/com/trelloiii/cibot/CiBotApplication.java
@@ -9,8 +9,22 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 public class CiBotApplication {
 
     public static void main(String[] args) {
+        setEnv(args);
         ApiContextInitializer.init();
         SpringApplication.run(CiBotApplication.class, args);
+    }
+
+    private static void setEnv(String[] args){
+        try{
+            System.setProperty("MYSQL_HOST",args[0]);
+            System.setProperty("MYSQL_USER",args[1]);
+            System.setProperty("MYSQL_PASSWORD",args[2]);
+            System.setProperty("BOT_TOKEN",args[3]);
+            System.setProperty("PORT",args[4]);
+        }
+        catch (NullPointerException | ArrayIndexOutOfBoundsException e){
+            System.out.println("environments not found");
+        }
     }
 
 }
